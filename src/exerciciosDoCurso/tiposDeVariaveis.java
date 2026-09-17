30abb62230317c107d8e0e12406282beb898d6b1
edu-hrq <eduardohenriquedasilva1096@gmail.com>
2026-09-17 15:49:33 -0300

    Segundo exercício: Tipos de Variáveis

---
+++ src/exerciciosDoCurso/tiposDeVariaveis.java
@@ -0,0 +1,20 @@
+package exerciciosDoCurso;
+
+// Crie um programa que declara e imprime variáveis de diferentes tipos (int, double, char, boolean, String).
+
+public class tiposDeVariaveis {
+    public static void main(String[] args) {
+
+        String nome = "José";
+        int idade = 22;
+        double peso = 98.5;
+        char genero = 'M';
+        boolean ativoNoSistema = true;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Peso: " + peso + "kgs");
+        System.out.println("Genero: " + genero);
+        System.out.println("Ativo no sistema: " + ativoNoSistema);
+    }
+}
