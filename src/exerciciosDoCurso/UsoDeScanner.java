35c46fd5b8d714b4da3d2c6454d93897199f965a
edu-hrq <eduardohenriquedasilva1096@gmail.com>
2026-09-17 15:53:19 -0300

    Exercício 3: Uso Simples do Scanner

---
+++ src/exerciciosDoCurso/UsoDeScanner.java
@@ -0,0 +1,16 @@
+package exerciciosDoCurso;
+
+// Use Scanner para receber um nome e exibir "Olá, [nome]!"
+
+import java.util.Scanner; // importação do Scanner
+
+public class UsoDeScanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Olá, " + nome + "!");
+    }
+}
