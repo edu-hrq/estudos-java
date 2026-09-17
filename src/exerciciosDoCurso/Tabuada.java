4e8f547f4db281b741331141e2f4f0464e6d4e2e
edu-hrq <eduardohenriquedasilva1096@gmail.com>
2026-09-17 16:09:26 -0300

    Sexto Exercício: Tabuada

---
+++ src/exerciciosDoCurso/Tabuada.java
@@ -0,0 +1,19 @@
+package exerciciosDoCurso;
+
+// Peça um número e imprima a tabuada de 1 a 10 com for.
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
