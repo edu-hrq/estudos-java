ff921c5e4debf9cc345c5ef2b751278a9113180c
edu-hrq <eduardohenriquedasilva1096@gmail.com>
2026-09-17 16:05:37 -0300

    Quinto Exercício: Contador com Loop For

---
+++ src/exerciciosDoCurso/LoopFor.java
@@ -0,0 +1,13 @@
+package exerciciosDoCurso;
+
+// Use um For para imprimir de 1 a 10.
+
+public class LoopFor {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
